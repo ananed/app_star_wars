8f3778e94653cb73898a7ed548a254205c2d721a
Anita <ana@codewithdragos.com>
2021-04-12 19:04:03 +0200

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,35 +1,49 @@
+import React, { useState, useEffect } from "react";
+import PeopleContainer from "./components/People";
 
-import React from "react";
-import People from './components/People';
-import {BrowserRouter as Router} from 'react-router-dom';
-
-function App(){
+function App() {
   //inicializo el estado inicial de esto
   const [people, setPeople] = useState([]);
- //voy a cambiar el estado con los datos de la API
+  const [next, setNext] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  //voy a cambiar el estado con los datos de la API
   useEffect(() => {
     //voy a poner la funcion fetch pero de otra forma
-    async function fetchPeople(){
-    let res = await fetch('https://swapi.py4e.com/api/people/?format=json');
-    let data = res.json();
-    setPeople(data.results);
+    async function fetchPeople() {
+      let res = await fetch("https://swapi.py4e.com/api/people");
+      let data = await res.json();
+      setNext(data.next);
+      setPeople(data.results);
     }
 
     fetchPeople();
-    }, [])
+  }, []);
 
-console.log('people', people);
-return(
-  <div className='App'>
-  <Route exact path='/people'>
-  <People data={people}/>
-  </Route>
-  </div>
-)
+  async function fetchNext(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    let res = await fetch(next);
+    let data = await res.json();
+    setNext(data.next);
+    setPeople([...people, ...data.results]);
+    setIsLoading(false);
+  }
+
+  //en return se utiliza (e) => fetchNext(e) para llamar a la funcion fetchNext con el evento e
+  return (
+    <div className="App">
+      <PeopleContainer data={people} />
+      {isLoading ? (
+        <button className="boton" disabled>
+          Loading ...
+        </button>
+      ) : (
+        <button className="boton" onClick={(e) => fetchNext(e)}>
+          Get more
+        </button>
+      )}
+    </div>
+  );
 }
-
 export default App;
-
-
-
-
--- my-app/src/components/People.js
+++ my-app/src/components/People.js
@@ -1,32 +1,34 @@
 import React from "react";
-import {Card, Grid} from 'semantic-ui-react';
+import { Grid } from "semantic-ui-react";
 
-
-export default function People(){
-      return(
-            <div>
-            <p>People</p>
-            <Grid columns={3}>
-             {data.map((people, i)=> {
-                   return(
-                         <Grid.Column key={i}>
-                         <Card>
-                         <Card.Content>
-                         <Card.Content>{people.name}</Card.Content>
-                         <Card.Description>
-                         <strong>Height</strong>
-                         <p>{people.height}</p>
-                         <strong>Birth year</strong>
-                         <p>{people.birth_year}</p>
-                         <strong>Films</strong>
-                         <p>{people.films+ "films"}</p>
-                         </Card.Description>
-                         </Card.Content>
-                         </Card>
-                         </Grid.Column>
-                   )
-             })}
-            </Grid>
-            </div>
-      );
+export default function People(props) {
+  return (
+    <div>
+      <p className="titulo">Star Wars Catalog</p>
+      <Grid columns={3}>
+        {props.data &&
+          props.data.map((people, i) => {
+            return (
+              <div className="titulo_card">
+                <strong className="nombre">{people.name}</strong>
+                <div className="other">
+                  <p className="elemento">
+                    <strong>Height: </strong>
+                    {people.height}
+                  </p>
+                  <p className="elemento">
+                    <strong>Birth Year: </strong>
+                    {people.birth_year}
+                  </p>
+                  <p className="elemento">
+                    <strong>Films: </strong>
+                    {people.films.length}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+      </Grid>
+    </div>
+  );
 }
