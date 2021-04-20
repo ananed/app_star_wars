09dce289ac73daf9f2acd029bae8e511c0b11379
Anita <ana@codewithdragos.com>
2021-04-20 11:10:19 +0200

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { Route, Switch } from "react-router-dom";
 import { Grid } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import Card from "./Card";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  
+  //se inicia con lista vacía
   const [people, setPeople] = useState([]);
   const [next, setNext] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -14,10 +14,10 @@ function App() {
   useEffect(() => {
     //voy a poner la funcion fetch pero de otra forma
     async function fetchPeople() {
-      let res = await fetch("https://swapi.py4e.com/api/people");
-      let data = await res.json();
+      let response = await fetch("https://swapi.py4e.com/api/people");
+      let data = await response.json();
       setNext(data.next);
-      setPeople(data);
+      setPeople(data.results);
     }
     fetchPeople();
   }, []);
@@ -34,32 +34,62 @@ function App() {
 
   return (
     <div className="all_all">
-      <p className="titulo">Star Wars Catalog</p>
-      <Grid columns={3}>
-        {people &&
-          people.map((people, i) => {
-            return (
-              <Link to={`/card/${people.id}`}>
-                <div> <people data={people} />
-                {isLoading ? (
-                  <button className="boton" disabled>
-                    Loading ...
-                  </button>
-                ) : (
-                  <button className="boton" onClick={(e) => fetchNext(e)}>
-                    Get more
-                  </button>
-                )}
-                ;</div>
-              </Link>
-            );
-          })}
-      </Grid>
-
-      <Switch>
-        <Route path="/card" element={Card}></Route>
-      </Switch>
+      <main>
+        <div className="titulo">Star Wars Catalog</div>
+        <Grid columns={3}>
+          {/* se pone solo people porque ya hemos metido en ella los data.results */}
+          {people &&
+            people.map((people, i) => {
+              return (
+                <Link to={`/Card/${i + 1}`}>
+                  <div className="row">
+                    <div className="nombre">
+                      <p>{people.name}</p>
+                    </div>
+                    <div className="col">
+                      <div>
+                        <p>
+                          Height: <strong> {people.height}</strong>
+                        </p>
+                        <p>
+                          Birth Year: <strong> {people.birth_year}</strong>
+                        </p>
+                      </div>
+                      <div>
+                        <p>
+                          Films: <strong> {people.films.length}</strong>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
+        </Grid>
+        <div>
+          {isLoading ? (
+            <button className="boton" disabled>
+              Loading ...
+            </button>
+          ) : (
+            <button className="boton" onClick={(e) => fetchNext(e)}>
+              Get more
+            </button>
+          )}
+          ;
+        </div>
+      </main>
     </div>
   );
 }
-export default App;
+//???
+function Root() {
+  return (
+    <Switch>
+      <Route path="/Card/:id" component={Card}></Route>
+      <Route path="/" component={App}></Route>
+    </Switch>
+  );
+}
+
+export default Root;
--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -1,8 +1,54 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Grid } from "semantic-ui-react";
 
 
+function Card() {
+  const [person, setPerson] = useState(null);
+  const { id } = useParams();
 
-export default function Card() {
-return (
-    <h1>Esto esta funcionando. Yuhu!</h1>
-);
+  useEffect(() => {
+    const url = `https://swapi.py4e.com/api/people/${id}`;
+    //voy a poner la funcion fetch pero de otra forma
+    async function fetchPerson() {
+      let response = await fetch(url);
+      let data = await response.json();
+      setPerson(data);
+   
+    }
+    fetchPerson();
+  }, [id]);
+
+  return (
+    <div className="all_all">
+    <main>
+      <div className="titulo">Star Wars Catalog</div>
+      <Grid columns={1}>
+        {person &&
+              <div className="personaje">
+                <div className="nombre">
+                  <p>{person.name}</p>
+                </div>
+                <div className="col">
+                  <div>
+                    <p>
+                      Height: <strong> {person.height}</strong>
+                    </p>
+                    <p>
+                      Birth Year: <strong> {person.birth_year}</strong>
+                    </p>
+                  </div>
+                  <div>
+                    <p>
+                      Films: <strong> {person.films.length}</strong>
+                    </p>
+                  </div>
+                </div>
+              </div>
+          })
+      </Grid>
+    </main>
+  </div>
+)
 }
+export default Card;
